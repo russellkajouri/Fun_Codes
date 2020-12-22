8d59a81173030e7a52ff50825f1f931ecc9eb512
rasoulkajouri <rasoulkajouri@gmail.com>
2020-12-23 00:01:45 +0100

    modified hpp file and add new feature to create a multi modes text

--- color/colors.hpp
+++ color/colors.hpp
@@ -4,10 +4,13 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
+#include <initializer_list>
 
 class colors {
 	private:
 
+		std::string text = "";
 		short color_counter;
 		enum color {
 			black = 1, 
@@ -87,14 +90,119 @@ class colors {
 
 	public:
 		std::string colorSet();
+		std::string colorSet(std::initializer_list <std::string>, std::string, std::string);
 		std::string colorSet(std::string, std::string, std::string);
+
+
 };
+
 
 // to set default 
 std::string colors :: colorSet(){
 	return "\e[0m";
 }
 
+// --------------------------------------------------------
+std::string colors :: colorSet(std::initializer_list<std::string> md, std::string cl, std::string op){
+
+
+	opacity opc = map_opacity [op];
+	color   clr = map_color   [cl];
+
+	text = "\e[";
+
+	// to find the opcaity of the color
+	switch(opc) {
+		case dark:
+			color_counter = 90;
+			break;
+		case light:
+			color_counter = 30;
+			break;
+		default:
+			color_counter = 0;
+	}
+
+	for (std::string x : md){
+	
+		mode mod = map_mode [x];
+
+		// to set mode of the text
+		switch (mod){
+		
+			case bold:
+				text += "1";
+				break;
+			case dim:
+				text += "2";
+				break;
+			case underlined:
+				text += "4";
+				break;
+			case blink:
+				text += "5";
+				break;
+			case inverted:
+				text += "7";
+				break;
+			case hidden:
+				text += "8";
+				break;
+			default:
+				text += "0";
+		
+		}
+
+		text += ";";
+	
+	}
+
+
+	// to set color of the text
+	switch(clr){
+		
+		case black:
+			color_counter = 30;
+			break;
+		case red:
+			color_counter += 1;
+			break;
+		case green:
+			color_counter +=2;
+			break;
+		case yellow:
+			color_counter +=3;
+			break;
+		case blue:
+			color_counter +=4;
+			break;
+		case magenta:
+			color_counter +=5;
+			break;
+		case cyan:
+			color_counter +=6;
+			break;
+		case gray:
+			color_counter = opc == dark ? 90 : 37;
+			break;
+		case white:
+			color_counter = 97;
+		default:
+			color_counter = 0;
+	}
+
+	text += std::to_string(color_counter) + "m";
+
+
+
+	return text;
+
+}
+
+
+// --------------------------------------------------------
+
+// to set oen option for each argument
 std::string colors :: colorSet(std::string md, 
 		std::string cl, std::string op){
 
@@ -102,7 +210,7 @@ std::string colors :: colorSet(std::string md,
 	color   clr = map_color   [cl];
 	mode    mod = map_mode    [md];
 
-	std::string text = "\e[";
+	text = "\e[";
 
 
 	// to find the opcaity of the color
@@ -117,7 +225,7 @@ std::string colors :: colorSet(std::string md,
 			color_counter = 0;
 	}
 
-	std::cout << color_counter << '\n';
+	//std::cout << color_counter << '\n';
 
 
 	// to set mode of the text
@@ -147,6 +255,7 @@ std::string colors :: colorSet(std::string md,
 	}
 
 	text += ";";
+
 	// to set color of the text
 	switch(clr){
 		
@@ -184,8 +293,5 @@ std::string colors :: colorSet(std::string md,
 
 	return text;
 }
-
-
-
 
 #endif // COLORS

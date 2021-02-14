473129cd7906c8be715ec2a7bdc92ce518bf55d0
rasoulkajouri <rasoulkajouri@gmail.com>
2021-02-14 07:01:11 +0100

    adding the SAME method to calculate the autocorr

--- autoCorrelation/acorr.py
+++ autoCorrelation/acorr.py
@@ -2,28 +2,32 @@
 #
 # @author : Kajo
 # @email  : rasoulkajouri@gmail.com
-# 
+#
 # @last modified : 14 Jan 2021
 #
 # -------------------------------------------------------------------
 
-def acorr(arr):
-    import math
-    # -------------------------------------------
-    # to find the average of the array
+# -------------------------------------------------------------------
+# to find the average of the array
+def Average(arr):
+	return sum(arr)/len(arr)
+# end of the average function
+# -------------------------------------------------------------------
 
-    ave_arr = (sum(arr) / len(arr))
-
-    # -------------------------------------------
-    # to find the correlate values
-
-    variance_arr = 0
-    for n in range(len(arr)):
-        variance_arr += (arr[n] - ave_arr) * (arr[n] - ave_arr)
-
-    # -------------------------------------------
-    # number of lags
-
+# -------------------------------------------------------------------
+# to find the variance of the array
+def Variance(arr):
+	ave = Average(arr)
+	var = 0.0
+	for n in range(len(arr)):
+		var += (arr[n]-ave)*(arr[n]-ave)
+	return var
+# end of the variance function
+# -------------------------------------------------------------------
+# to find the autocorrelation series under the full mode
+def FullAcorr(arr, ave_arr, variance_arr):
+	# -------------------------------------------
+	# number of lags
     numLags = len(arr)
 
     # -------------------------------------------
@@ -50,3 +54,56 @@ def acorr(arr):
 
     lWing = list(reversed(lWing))
     return lWing + rWing
+# the end of the FullAcorr function
+# -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
+# to find the autocorrelation series under the same mode
+def SameAcorr(arr, ave_arr, variance_arr):
+	# -------------------------------------------
+	# number of lags
+    numLags = len(arr)
+
+    # -------------------------------------------
+    # left and right wings
+
+    rWing = [0] * len(arr)
+
+    # -------------------------------------------
+    # to find the auto correlation on left wing
+
+    for lag in range(numLags):
+        numerator = 0.0
+        for n in range(len(arr) - lag):
+            numerator += (arr[n] - ave_arr) * (arr[n+lag] - ave_arr)
+        rWing[lag] = numerator / variance_arr
+        pass
+    return rWing
+# the end of the FullAcorr function
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+def Acorr(arr, mode="same"):
+	import math
+# -------------------------------------------
+# to find the variance
+
+	average = Average(arr)
+	variance = Variance(arr)
+
+# -------------------------------------------
+# to recognize the mode
+
+	FULL = "full"
+	SAME = "same"
+
+# -------------------------------------------
+# to calculate the auto correlation 
+
+	if mode == FULL:
+		return FullAcorr(arr, average, variance)
+	elif mode == SAME:
+		return SameAcorr(arr, average, variance)
+	else:
+		return []
+# the end of the acorr function
+# -------------------------------------------------------------------
